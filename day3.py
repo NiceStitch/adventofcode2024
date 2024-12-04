789dc694c8415006509648c82452f4a515da0779
Wojtek MEDYK <146181611+NiceStitch@users.noreply.github.com>
2024-12-04 14:49:21 +0100

    Update day3.py
    Added part 2

--- day3.py
+++ day3.py
@@ -1,9 +1,6 @@
 # Part 1
 import re
 
-def mul(a, b):
-    return a * b
-
 with open('input2.txt', 'r') as file:
     content = file.read()  
 
@@ -17,3 +14,33 @@ for match in matches:
     sum += a * b  
 
 print(f"Total sum of all mul results: {sum}")
+# Part 2
+def process_instructions(text):
+    enabled = True #Enabled by default for the first line
+    total_sum = 0
+    
+    
+    do_dont_regex = r'do\(\)|don\'t\(\)'
+    # Split on do or don't
+    instructions = re.split(f'({do_dont_regex})', text)
+    for instruction in instructions:
+        instruction = instruction.strip() # Remove whitespaces just to be sure
+        
+        if instruction == "do()": # Switch to true if do()
+            enabled = True
+        
+        elif instruction == "don't()": # Switch to false if don't
+            enabled = False
+        
+        else: # Iterate thorough all the slices, if it comes after do() enabled is true and it will be counted, if after don't enabled is false so it won't be included in the count
+            mul_matches = re.findall(regex, instruction)
+            for match in mul_matches:
+                if enabled:
+                    a, b = map(int, match)
+                    total_sum += a * b
+    
+    return total_sum
+
+text = content # Lazy fix to reimplmement content after testing 
+result = process_instructions(text)
+print(f"Sum of enabled multiplications: {result}")
